ee3d587e2c0fecbbbc60bad5197db2b2e54decef
Parker Hodge <iparkerhodge@gmail.com>
2020-06-30 14:30:04 -0500

    created post, profile, and comments APIs

---
+++ Gifter/Controllers/UserProfileController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Gifter.Data;
+using Gifter.Repositories;
+using Gifter.Models;
+
+namespace Gifter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly UserProfileRepository _profileRepository;
+        public UserProfileController(ApplicationDbContext context)
+        {
+            _profileRepository = new UserProfileRepository(context);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_profileRepository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var post = _profileRepository.GetById(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+
+        [HttpPost]
+        public IActionResult UserProfile(UserProfile post)
+        {
+            _profileRepository.Add(post);
+            return CreatedAtAction("Get", new { id = post.Id }, post);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UserProfile post)
+        {
+            if (id != post.Id)
+            {
+                return BadRequest();
+            }
+
+            _profileRepository.Update(post);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _profileRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
